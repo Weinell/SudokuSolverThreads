e340be56abe816d1dd381ce3f655b83ba043c88d
Nikolaj <nikolajweinell@hotmail.com>
2021-09-18 18:28:37 +0200

    Created void methods for reader

--- src/Reader.java
+++ src/Reader.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+
 public class Reader {
 
-    public Reader() {
+    ArrayList<Integer> reader;
+
+    public Reader(ArrayList<Integer> reader) {
+
+        this.reader = reader;
+
     }
+
+    public void readHorizontal()    {
+
+    }
+
+    public void readVertical()  {
+
+    }
+
+    public void readBox()   {
+        
+
+    }
+
+
 }
